cb7be568da37c8f33ee95a67b22c13b82f94e1f5
Ramirez-Benmar <134592419+Ramirez-Benmar@users.noreply.github.com>
2024-02-12 00:26:08 +0800

    Adding Views
    + views
    + finished navbar
    +dropdowns
    +routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App';
 import Navbar from './Components/Navbar/Navbar.jsx'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './Components/Views/Home.jsx';
+import Products from './Components/Views/Products.jsx';
+import Partners from './Components/Views/Partners.jsx';
+import About from './Components/Views/About.jsx';
+import Cart from './Components/Views/Cart.jsx';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Products category="list"/>}/>
+        <Route path='/' element={<Products category="search"/>}/>
+        <Route path='/' element={<Partners category="resellers"/>}/>
+        <Route path='/' element={<Partners category="sponsors"/>}/>
+        <Route path='/' element={<About category="contact"/>}/>
+        <Route path='/' element={<About category="faq"/>}/>
+        <Route path='/' element={<About category="terms"/>}/>
+        <Route path='/' element={<About category="refund"/>}/>
+        <Route path='/' element={<Cart/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,74 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import './Navbar.css';
 import logoMain from '../../Assets/logo_main.png';
 import cartIcon from '../../Assets/cart_icon.png';
+import menuIcon from '../../Assets/menu.png';
+import searchIcon from '../../Assets/search.png';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="nav-logo">
-          <img src={logoMain} alt="" className='logo-img' />
-          <p>Adobo Flakes</p>
+        <img src={logoMain} alt="" className='logo-img' />
       </div>
       <ul className="nav-menu">
-          <li>Home</li>
-          <li>Products</li>
-          <li>FAQ</li>
-          <li>Contact Us</li>
-          <li>Partners</li>
+        <li className="nav-item" onClick={() => redirectTo('/')}>Home</li>
+
+        <DropdownButton title="Products">
+          <DropdownMenu>
+            <button className="dropdown-item" onClick={() => redirectTo('/products')}>Product List</button>
+            <button className="dropdown-item" onClick={() => redirectTo('/search')}>Search Product</button>
+          </DropdownMenu>
+        </DropdownButton>
+        <DropdownButton title="Partners">
+          <DropdownMenu>
+            <button className="dropdown-item" onClick={() => redirectTo('/resellers')}>Resellers</button>
+            <button className="dropdown-item" onClick={() => redirectTo('/sponsors')}>Sponsors</button>
+          </DropdownMenu>
+        </DropdownButton>
+        <DropdownButton title="About">
+          <DropdownMenu>
+            <button className="dropdown-item" onClick={() => redirectTo('/contact')}>Contact Us</button>
+            <button className="dropdown-item" onClick={() => redirectTo('/faq')}>Frequently Asked</button>
+            <button className="dropdown-item" onClick={() => redirectTo('/terms')}>Terms of Service</button>
+            <button className="dropdown-item" onClick={() => redirectTo('/refund')}>Refund Policy</button>
+          </DropdownMenu>
+        </DropdownButton>
       </ul>
+
       <div className="nav-login-cart">
-          <button>Login</button>
-          <img src={cartIcon} alt="" className='cart-img' />
+        <img src={cartIcon} alt="" className='cart-img' />
+        <div className="nav-cart-count">0</div>
+        <img src={searchIcon} alt="" className='search-img' />
+        <img src={menuIcon} alt="" className='menu-img' />
       </div>
     </div>
-    
   )
 }
 
-export default Navbar
+const redirectTo = (route) => {
+  window.location.href = route;
+};
+
+const DropdownButton = ({ title, children }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <li className="dropdown-button" onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
+      {title}
+      {isOpen && children}
+    </li>
+  );
+};
+
+const DropdownMenu = ({ children }) => (
+  <div className="dropdown-menu">
+    {children}
+  </div>
+);
+
+export default Navbar;
